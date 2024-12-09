393da73dd346d9fb702cfe07f7c7302ced4718de
ALA BOUDEBIA <108654196+Ala-Dine@users.noreply.github.com>
2024-12-09 20:19:33 +0100

    Update Tp2.c

--- Tp2.c
+++ Tp2.c
@@ -1,49 +1,69 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-char *ChargerChaine(int N) {
-    char *chaine = (char*)malloc(N+1); 
-    printf("Veuillez saisir la chaine:\n");
-    fgets(chaine, N, stdin);
-    chaine[strcspn(chaine, "\n")] = '\0'; 
-    return chaine;
-}
-
-int Longueur(char *ch) {
-    return strlen(ch);
-}
-
-void ChargerTab(char *p, char Tab[]) {
-    strcpy(Tab, p);
-}
-
-void InverserTab(char Tab[], char T[], int m) {
-    for (int i = 0; i < m; i++) {
-        T[i] = Tab[m-i-1];
-    }
-}
-
-void AfficherTab(char Tab[], int m) {
-    for (int i = 0; i < m; i++) {
-        printf("%c", Tab[i]);
-    }
-    printf("\n");
-}
+// Function prototypes
+char *LoadString(int N);
+int StringLength(char *str);
+void ReverseArray(char Array[], char Reversed[], int length);
+void LoadArray(char *str, char Array[]);
+void DisplayArray(char Array[], int length);
 
 int main() {
+    char *str;
     int n;
-    printf("Veuillez saisir la taille maximale de la chaine:\n");
+    printf("Please enter the maximum size of the string: ");
     scanf("%d", &n);
-    char *ch = ChargerChaine(n);
-    int m = Longueur(ch);
-    char Tab[m], T[m];
-    ChargerTab(ch, Tab);
-    printf("La chaine originale: \n");
-    AfficherTab(Tab, m);
-    InverserTab(Tab, T, m);
-    printf("La chaine inversée:\n");
-    AfficherTab(T, m);
-    free(ch);
+    str = LoadString(n);
+    int length = StringLength(str);
+    char Array[length], Reversed[length];
+    LoadArray(str, Array);
+    DisplayArray(Array, length);
+    ReverseArray(Array, Reversed, length);
+    DisplayArray(Reversed, length);
+    free(str);
     return 0;
+}
+
+// Function definitions
+
+// Function to load a string of maximum size N
+char *LoadString(int N) {
+    char *str = (char *)malloc(N * sizeof(char));
+    printf("Please enter the string: ");
+    scanf("%s", str);
+    return str;
+}
+
+// Function to get the length of a string
+int StringLength(char *str) {
+    int string_length = 0;
+    while(str[string_length] != '\0'){
+        string_length++;
+    }
+    return string_length;
+}
+
+// Function to reverse the array
+void ReverseArray(char Array[], char Reversed[], int length) {
+    for (int i = 0; i < length; i++) {
+        Reversed[i] = Array[length - 1 - i];
+    }
+}
+
+// Function to load the array from the string
+void LoadArray(char *str, char Array[]) {
+    int i = 0;
+    while(str[i] != '\0'){
+        Array[i] = str[i];
+        i++;
+    }
+    Array[i] = '\0';
+}
+
+// Function to display the array
+void DisplayArray(char Array[], int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%c", Array[i]);
+    }
+    printf("\n");
 }
